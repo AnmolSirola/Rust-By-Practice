7ce4cebf77c82ce66b4198f14e779e079e76a0a2
Anmol Sirola <95535448+AnmolSirola@users.noreply.github.com>
2023-08-05 21:01:06 +0530

    Add files via upload

---
+++ condtionals.rs
@@ -0,0 +1,14 @@
+/*fn main(){
+
+    let cond = 2 < 3;
+
+    println!("{}", cond);
+}
+*/
+
+fn main(){
+
+    let cond = (2 as f32) < 3.3;
+
+    println!("{}", cond);
+}
---
+++ dataTypes.rs
@@ -0,0 +1,10 @@
+fn main(){
+    println!("Hello World");
+
+    let floating_point = 10.92
+    let true_or_false: bool = true
+    let letter: char = "a"
+    
+    let tup: (i32, bool, char) = (1, true, "b");
+    println("{}, tup.2"); // b
+}
